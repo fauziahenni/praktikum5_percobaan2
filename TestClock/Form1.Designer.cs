d61bf5db7c17bd6d16017ab6957f5e531f3610e2
fauziahenni <87329906+fauziahenni@users.noreply.github.com>
2021-12-10 06:33:19 -0800

    Add files via upload

---
+++ TestClock/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TestClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.digitalClock1 = new P5__2_1204013.DigitalClock();
+            this.UniversalTimeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Local Time";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Universal Time";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "start/button";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // digitalClock1
+            // 
+            this.digitalClock1.LocalTimerLabel_BackColor = System.Drawing.SystemColors.ControlDark;
+            this.digitalClock1.Location = new System.Drawing.Point(95, 26);
+            this.digitalClock1.Name = "digitalClock1";
+            this.digitalClock1.Size = new System.Drawing.Size(186, 59);
+            this.digitalClock1.TabIndex = 4;
+            this.digitalClock1.Timer1_Enabled = true;
+            this.digitalClock1.RaiseTimer1_Tick += new System.EventHandler(this.digitalClock1_RaiseTimer1_Tick_1);
+            // 
+            // UniversalTimeLabel
+            // 
+            this.UniversalTimeLabel.Location = new System.Drawing.Point(120, 141);
+            this.UniversalTimeLabel.Name = "UniversalTimeLabel";
+            this.UniversalTimeLabel.Size = new System.Drawing.Size(100, 23);
+            this.UniversalTimeLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.UniversalTimeLabel);
+            this.Controls.Add(this.digitalClock1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private P5__2_1204013.DigitalClock digitalClock1;
+        private System.Windows.Forms.Label UniversalTimeLabel;
+    }
+}
+
